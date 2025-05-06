137cd067e33f69275fbd24b516dd142dae0fda00
machineHouseWSL <andremacielcontato@gmail.com>
2025-05-05 22:40:02 -0300

    aula_08_layout

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,9 @@ Rails.application.routes.draw do
   get "produtos" => "produtos#index"
   root "produtos#index"
 
+  get "/produtos/new" => "produtos#new"
+  post "/produtos" => "produtos#create"
+
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker

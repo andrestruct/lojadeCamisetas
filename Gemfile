dc4bd0d1fecd9bab324dfcfa00d2e83125807250
machineHouseWSL <andremacielcontato@gmail.com>
2025-05-16 22:43:20 -0300

    validação

--- Gemfile
+++ Gemfile
@@ -43,6 +43,8 @@ gem "sassc-rails"
 
 gem "bootstrap-navbar"
 
+# gem para traduzir alertas
+gem "i18n"
 
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
 # gem "image_processing", "~> 1.2"
--- app/controllers/produtos_controller.rb
+++ app/controllers/produtos_controller.rb
@@ -4,6 +4,12 @@ class ProdutosController < ApplicationController
     @produtos_por_preco = Produto.order :preco
   end
 
+
+  def new
+    @produto = Produto.new
+  end
+
+
   def busca
     # nome_a_buscar = "%#{params[:nome]}%"
     # @produtos = Produto.where "nome like ?", nome_a_buscar
@@ -15,8 +21,15 @@ class ProdutosController < ApplicationController
 
   def create
     valores = params.require(:produto).permit :nome, :preco, :descricao, :quantidade
-    produto = Produto.create valores
-    redirect_to root_url
+
+    @produto = Produto.new valores
+
+    if @produto.save
+      flash[:notice] = "Produto salvo com sucesso"
+      redirect_to root_url
+    else
+      render :new
+    end
   end
 
   def destroy
